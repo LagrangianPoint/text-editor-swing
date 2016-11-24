14609d85f4f1e7dbdda7ca1eb24a291409f14d81
alvaro <zephyr.3@gmail.com>
2016-11-24 02:24:22 -0700

    Added second wave of files for this editor.

---
+++ src/simpletexteditor/Main.java
@@ -0,0 +1,15 @@
+package simpletexteditor;
+
+/**
+ * This is yet another simple text editor
+ * @author https://github.com/LagrangianPoint
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String input[]  = {""};
+        SimpleTextEditor.main(input);
+        
+    }
+    
+}
